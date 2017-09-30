af037b46bc29b1820a6e41ecda96e92b7f827d49
Sandro Coelho <sandro.icoelhos@gmail.com>
2017-09-30 14:53:32 -0300

    first up

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -4,5 +4,9 @@ package Main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
     }
 }
