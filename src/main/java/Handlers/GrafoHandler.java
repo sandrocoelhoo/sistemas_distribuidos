dce9fa25e7527f8c98ce2f9917b4ec586624dc4b
Sandro Coelho <sandro.icoelhos@gmail.com>
2017-10-02 20:18:08 -0300

    Teste de conexão server/client

--- src/main/java/Handlers/GrafoHandler.java
+++ src/main/java/Handlers/GrafoHandler.java
@@ -39,12 +39,18 @@ public class GrafoHandler implements MetodosGrafo.Iface{
 
     @Override
     public boolean updateVertice(Vertice v, int cor) throws KeyNotFound, TException {
-        return true;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public boolean deleteVertice(Vertice v) throws KeyNotFound, TException {
+        Vertice vertice;
+        for (Integer key : v.HashAresta.keySet()) {
+            vertice = HashVertice.get(v.HashAresta.get(key).nomeVertice1);
+            vertice.HashAresta.remove(key);
+        }
         HashVertice.remove(v.nome);
+
         return true;   
     }
 
@@ -62,9 +68,9 @@ public class GrafoHandler implements MetodosGrafo.Iface{
     @Override
     public List<Vertice> readVerticeNeighboors(Vertice v) throws TException {
         ArrayList<Vertice> Vertices = new ArrayList<>();
-        
-        for (Integer key : v.HashAresta.keySet()) {
-            Vertices.add(HashVertice.get(v.HashAresta.get(key).nomeVertice));
+        Vertice vertice = HashVertice.get(v);
+        for (Integer key : vertice.HashAresta.keySet()) {
+            Vertices.add(HashVertice.get(key));
         }
         
         return Vertices;
@@ -72,20 +78,31 @@ public class GrafoHandler implements MetodosGrafo.Iface{
 
     @Override
     public boolean addAresta(Aresta a, Vertice v) throws TException {
-        HashVertice.get(v.nome).HashAresta.put(v.nome, a);
-        
-        
+        HashVertice.get(v.nome).HashAresta.put(v.nome, a);      
         return true;
     }
 
     @Override
     public List<Aresta> readAllAresta() throws TException {
-        
+        ArrayList<Aresta> Arestas = new ArrayList<>();
+
+        for (Integer keyVertice : HashVertice.keySet()) {
+            for (Integer keyAresta : HashVertice.get(keyVertice).HashAresta.keySet()) {
+                Arestas.add(HashVertice.get(keyVertice).HashAresta.get(keyAresta));                
+            }
+        }
+        return Arestas;
     }
 
     @Override
     public List<Aresta> readAllArestaOfVertice(Vertice v) throws TException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<Aresta> Arestas = new ArrayList<>();
+        
+        for (Integer key : v.HashAresta.keySet()) {
+            Arestas.add(v.HashAresta.get(key));
+        }
+        
+        return Arestas;
     }
 
     @Override
@@ -95,11 +112,12 @@ public class GrafoHandler implements MetodosGrafo.Iface{
 
     @Override
     public boolean deleteAresta(Aresta a) throws KeyNotFound, TException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean addAresta(Aresta a, Vertice v) throws TException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Vertice v1 = HashVertice.get(a.nomeVertice1);
+        Vertice v2 = HashVertice.get(v1.HashAresta.get(a.nomeVertice1));
+        
+        v1.HashAresta.remove(a);
+        v2.HashAresta.remove(a);
+        
+        return true;
     }
 }
